33966528f62e800fed983dec7da9a7da9944c647
Luis Ignacio Ramirez Martinez <liramirez@ayesa.es>
2020-09-05 18:58:47 -0500

    Indice de masa corporal

--- suma.py
+++ suma.py
@@ -4,3 +4,13 @@ n = int(input(': '))
 suma = n * (n + 1)/2
 
 print("La suma de los n primeros numeros naturales es: ",int(suma))
+
+#-----------------Ejercicio 9------------------------------------------
+#Calculo de indice de masa corporal
+
+weight = input("Cual es su peso en kg: ")
+height = input("¿Cuál es su estatura en metros: ?")
+bmi = round(float(weight)/float(height)**2,2)
+print("Su indice de masa corporal es: "+str(bmi))
+
+
