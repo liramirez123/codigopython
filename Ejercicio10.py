2c4cbd596f5e09af4f98b93b70a78e3f227f2ad1
Luis Ignacio Ramirez Martinez <liramirez@ayesa.es>
2020-09-05 21:19:27 -0500

    nuevos programas

---
+++ Ejercicio10.py
@@ -0,0 +1,3 @@
+n = input("Introduce el dividendo(Entero): ")
+m = input("Introduce el divisor(entero): ")
+print(n + " entre "+ m + " da un cociente de "+ str(int(n)//int(m))+ " y un resto "+str(int(n)%int(m)) ) 
---
+++ mayorEdad.py
@@ -0,0 +1,5 @@
+age = int(input("¿Cual es su edad? "))12
+if age < 18:
+    print("Eres menor de edad ")
+else:
+    print("Eres mayor de edad")
